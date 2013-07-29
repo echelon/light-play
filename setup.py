e858e4b5609079eea380a19c4c07f02afe6c6e41
Brandon Thomas <bt@brand.io>
2013-07-29 03:28:43 -0400

    distutils

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name='lightplayground',
+	version='1.0dev',
+	packages=['lightplayground',],
+	license='BSD or something',
+	long_description=open('README.md').read(),
+)
+
