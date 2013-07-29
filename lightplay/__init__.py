d4afc3ad6739ddb690159b650d6d7c90e1efbf6a
Brandon Thomas <bt@brand.io>
2013-07-29 03:36:50 -0400

    Import names into module namespace.

---
+++ lightplay/__init__.py
@@ -0,0 +1,13 @@
+"""
+Setup LightPlay namespace.
+"""
+from dac import DAC
+from stream import PointStream
+from controller import Controller, XboxController, Ps3Controller
+from shape import shape
+from surface import Surface
+from entities.entity import Entity
+
+# TODO: GML
+# TODO: SVG
+# TODO: Entities 
