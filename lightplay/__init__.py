a5c422a5c218e6e58fb0eb8db8135b6f8093a8da
Brandon Thomas <bt@brand.io>
2013-07-29 03:39:09 -0400

    Fix imports

--- lightplay/__init__.py
+++ lightplay/__init__.py
@@ -4,10 +4,10 @@ Setup LightPlay namespace.
 from dac import DAC
 from stream import PointStream
 from controller import Controller, XboxController, Ps3Controller
-from shape import shape
+from shape import Shape
 from surface import Surface
-from entities.entity import Entity
 
 # TODO: GML
 # TODO: SVG
 # TODO: Entities 
+#from entities.entity import Entity
