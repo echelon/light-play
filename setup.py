ad3b74e34258a8995e48405383d678e69b7e2818
Brandon Thomas <bt@brand.io>
2013-07-29 03:46:32 -0400

    chmod+x; pygame requirement

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from distutils.core import setup
 
 setup(
@@ -6,5 +8,8 @@ setup(
 	packages=['lightplay',],
 	license='BSD or something',
 	long_description=open('README.md').read(),
+	author = 'Brandon Thomas',
+	author_email = 'bt@brand.io',
+	requires = ['pygame(>=1.9.0)'],
 )
 
